4ca044579b4feb33318c91d3693727e61e6ae302
Nicola Piemontese <nicola.piemontese@cpe.fr>
2023-12-18 19:41:02 +0100

    init ms-content

---
+++ ms-content/code/main.py
@@ -0,0 +1,101 @@
+
+import base64
+import os
+import json
+#from file import *
+from bucket_call import *
+import pika, os, sys
+import mailparser
+
+
+def main():
+    print("Receive")
+    host = os.getenv('RABBITMQ_HOST')
+    port = os.getenv('RABBITMQ_PORT')
+    user = os.getenv('RABBITMQ_USER')
+    password = os.getenv('RABBITMQ_PASSWORD')
+    queueSend = os.getenv('RABBITMQ_QUEUE', "sentimail")
+    virtualHost = os.getenv('RABBITMQ_VHOST', "/")
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=host,
+            port=port,
+            virtual_host=virtualHost,
+            credentials=pika.PlainCredentials(user, password)
+            )
+        )
+
+    channel = connection.channel()
+    channel.queue_declare(queue=queueSend)
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % json.loads(body))
+        # récupérer uniquement la chaine de caractère entre les quotes du body
+
+        file = json.loads(body)
+        mailResult, ipResult, spfResult = analyse(file)
+        os.remove(file)
+        
+        #send_result(mailResult, ipResult, spfResult, file)
+        #send_result("A", "B", "C", file)
+
+    channel.basic_consume(queue=queueSend, on_message_callback=callback, auto_ack=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+def analyse(id_file):
+    # Initiation de la connexion avec le bucket en fonction de l'ID de l'objet et téléchargement du fichier
+    bucket_call(id_file)
+    mail = parseFile(id_file)
+    """  print(mail)
+    print("body", mail.body)
+    print("from", mail.from_)
+    print("to", mail.to)
+    print("subject", mail.subject)
+    print("date", mail.date)
+    print("server", mail.get_server_ipaddress) """
+    #(mailAnalysis, ipAnalysis, spfAnalysis) = analyse_file(id_file)
+    mailAnalysis = "A"
+    ipAnalysis = "B"
+    spfAnalysis = "C"
+    return mailAnalysis, ipAnalysis, spfAnalysis
+
+def parseFile(id_file):
+    mail = mailparser.parse_from_file(id_file)
+    return mail
+
+
+def send_result(mailResult, ipResult, spfResult, uuid):
+    # Send result to the API:  
+    print("Send result")
+    print("Mail: ", mailResult)
+    print("IP: ", ipResult)
+    print("SPF: ", spfResult)
+    user_metadata = os.getenv("MS_METADATA_USER")
+    password_metadata = os.getenv("MS_METADATA_PASSWORD")
+
+    # PATCH http://127.0.0.1:8000/api/analysis/uuid/
+    data = {
+            "responseMetadataIp": ipResult,
+            "responseMetadataDomain": mailResult,
+            "responseMetadataSPF": spfResult,
+        }
+    headers = {
+        #"Authorization": "Token " + os.getenv("API_KEY"),
+        "Authorization": "Basic " + base64.b64encode(bytes(user_metadata + ":" + password_metadata, "utf-8")).decode("ascii"),
+    }
+    url = "http://" + os.getenv("BACKEND_HOST", "127.0.0.1:8000") + "/api/analysis/" + uuid + "/"
+    request = requests.patch(url, data = data, headers = headers)
+    print("URL: ", url)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
