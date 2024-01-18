21144e59ffb5ed508bcd95656d8b8504070afa6f
Nicola Piemontese <nicola.piemontese@cpe.fr>
2024-01-18 12:28:56 +0100

    Add comments for ms-attachment

--- ms-attachment/code/check_filetype.py
+++ ms-attachment/code/check_filetype.py
@@ -1,2 +1,6 @@
 def check_filetype(mail):
+    """Check if there are malicious file types in the mail
+    :param mail: mail object
+    :return: "Clean" or "Malicious"
+    """
     return "Malicious"
