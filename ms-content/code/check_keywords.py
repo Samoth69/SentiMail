8a90738142c4cb9bd0974b901ef4962c6d5d17cb
Nicola Piemontese <nicola.piemontese@cpe.fr>
2024-01-22 11:46:49 +0100

    Refactor keyword checking logic in check_keywords.py

--- ms-content/code/check_keywords.py
+++ ms-content/code/check_keywords.py
@@ -33,52 +33,54 @@ def check_keywords(mail):
 
     content = mail.body
     subject = mail.subject
-    #print(content)
 
     content = content.lower()
     nb_words = len(content.split())
-
-    spam_score = 0
-    phishing_score = 0
-    spam_score_content = 0
-    phishing_score_content = 0
-    spam_score_subject = 0
-    phishing_score_subject = 0
-
-    for keyword in spam_keywords:
-        if re.search(r'\b' + re.escape(keyword) + r'\b', content):
-            spam_score_content += 1
-    
-    for keyword in phishing_keywords:
-        if re.search(r'\b' + re.escape(keyword) + r'\b', content):
-            phishing_score_content += 1
-    
-    for keyword in spam_keywords:
-        if re.search(r'\b' + re.escape(keyword) + r'\b', subject):
-            spam_score_subject += 1
-    
-    for keyword in phishing_keywords:
-        if re.search(r'\b' + re.escape(keyword) + r'\b', subject):
-            phishing_score_subject += 1
-    
-    # Average of the two scores
-    spam_score = (spam_score_content + spam_score_subject) / 2
-    phishing_score = (phishing_score_content + phishing_score_subject) / 2
-
-    # check percentage of spam and phishing keywords
-    spam_percentage = spam_score_content / nb_words * 100
-    phishing_percentage = phishing_score_content / nb_words * 100
-    ("Spam percentage: " + str(spam_percentage))
-    logger.info("Phishing percentage: " + str(phishing_percentage))
-
-    if spam_score > 0 and spam_score > phishing_score:
-        logger.info("End: Spam (score: " + str(spam_score) + ")")
-        return "Spam"
-    elif phishing_score > 0 and phishing_score > spam_score:
-        logger.info("End: Phishing (score: " + str(phishing_score) + ")")
-        return "Phishing"
-    else:
-        logger.info("End: Clean")
+    if nb_words == 0:
+        logger.info("End: Clean (no words)")
         return "Clean"
+    else:
+        spam_score = 0
+        phishing_score = 0
+        spam_score_content = 0
+        phishing_score_content = 0
+        spam_score_subject = 0
+        phishing_score_subject = 0
+
+        for keyword in spam_keywords:
+            if re.search(r'\b' + re.escape(keyword) + r'\b', content):
+                spam_score_content += 1
+        
+        for keyword in phishing_keywords:
+            if re.search(r'\b' + re.escape(keyword) + r'\b', content):
+                phishing_score_content += 1
+        
+        for keyword in spam_keywords:
+            if re.search(r'\b' + re.escape(keyword) + r'\b', subject):
+                spam_score_subject += 1
+        
+        for keyword in phishing_keywords:
+            if re.search(r'\b' + re.escape(keyword) + r'\b', subject):
+                phishing_score_subject += 1
+        
+        # Average of the two scores
+        spam_score = (spam_score_content + spam_score_subject) / 2
+        phishing_score = (phishing_score_content + phishing_score_subject) / 2
+
+        # check percentage of spam and phishing keywords
+        spam_percentage = spam_score_content / nb_words * 100
+        phishing_percentage = phishing_score_content / nb_words * 100
+        ("Spam percentage: " + str(spam_percentage))
+        logger.info("Phishing percentage: " + str(phishing_percentage))
+
+        if spam_score > 0 and spam_score > phishing_score:
+            logger.info("End: Spam (score: " + str(spam_score) + ")")
+            return "Spam"
+        elif phishing_score > 0 and phishing_score > spam_score:
+            logger.info("End: Phishing (score: " + str(phishing_score) + ")")
+            return "Phishing"
+        else:
+            logger.info("End: Clean")
+            return "Clean"
 
 
